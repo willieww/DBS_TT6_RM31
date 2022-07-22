18c13fd3133877db69bafa7c8bf2c9fc4851e2c0
Willie <willieww2511@gmail.com>
2022-07-22 16:28:52 +0800

    Edit color

--- mulitcurrencywallet/src/components/Navbar.js
+++ mulitcurrencywallet/src/components/Navbar.js
@@ -7,20 +7,20 @@ function NavigationBar() {
     localStorage.setItem('loggedIn', false);
   }
   return (
-    <Navbar bg="transparent" expand="lg" className={styles.navbar}>
+    <Navbar bg="black" expand="lg" className={styles.navbar}>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/Home" style={{ color: "black" }}>
+          <Nav.Link href="/Home" style={{ color: "white" }}>
             Home
           </Nav.Link>
-          <Nav.Link href="/Wallet" style={{ color: "black" }}>
+          <Nav.Link href="/Wallet" style={{ color: "white" }}>
             Wallet 
           </Nav.Link>
-          <Nav.Link href="/Transaction" style={{ color: "black" }}>
+          <Nav.Link href="/Transaction" style={{ color: "white" }}>
             Transaction 
           </Nav.Link>
-          <Nav.Link href="/" onClick={onLogout} style={{ color: "black" }}>
+          <Nav.Link href="/" onClick={onLogout} style={{ color: "white" }}>
             Logout
           </Nav.Link>
         </Nav>
