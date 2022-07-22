cf7e05d6394bce72d1eb90d8d70f6939d3738724
joyce-lim <joyce.lim2@dgi.sg>
2022-07-22 12:35:13 +0800

    Add login page

--- mulitcurrencywallet/src/LoginPage/index.js
+++ mulitcurrencywallet/src/LoginPage/index.js
@@ -1,24 +1,44 @@
 import React, {useState} from 'react';
+import './LoginPage.css';
+import DbsLogo from './assets/dbs_logo.png';
 
 const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const onChangeUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
     
     const onLogin = () => {
         // call backend to check for login
-        console.log('login button clicked');
+        console.log('login button clicked', username, password);
       }
   return (
-    <div>
-        <label>Username</label>
-        <input type="text" name="username" />
+    <div className="login-container"> 
+        <div className="input-container">
+            <img src={DbsLogo} className="image" />
+            <div className="sep-container">
+                <label className="label-text">User ID</label>
+                <input type="text" name="username" className="input" onChange={onChangeUsername} value={username} />
+            </div>
 
-        <label>Password</label>
-        <input type="password" name="password" />
+            <div className="sep-container">
+                <label className="label-text">Password</label>
+                <input type="password" name="password" className="input" onChange={onChangePassword} value={password} />
+            </div>
 
-        <button type="submit" onClick={onLogin}>Login</button>
-        
+            <button type="submit" className="login-button" onClick={onLogin}>
+                <h2 className="login-text">Login</h2>
+            </button>
+
+        </div>
     </div>
+    
   )
 }
 
